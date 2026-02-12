f073132d35248ead5da9b76f44e1876bf706c8a4
industrialNettalco <industrial@nettalco.com.pe>
2026-02-12 14:17:05 -0500

    update

--- Arweave/buscador_json.py
+++ Arweave/buscador_json.py
@@ -1,5 +1,5 @@
 from get_tickbar_data import get_clean_json_from_tickbar, save_json_to_file
 
-mi_json=get_clean_json_from_tickbar("092583907531")
+mi_json=get_clean_json_from_tickbar("092746313179")
 
 save_json_to_file(mi_json,"json_rescatados.json")
